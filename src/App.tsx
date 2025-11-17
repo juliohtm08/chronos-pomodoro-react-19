87c83cd2538f9ac02629dd28e0a1b459d602a868
juliohtm08 <jhtomoyuki345@gmail.com>
2025-11-17 16:46:16 -0300

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Heading } from './components/Heading';
 
+import { TimerIcon } from 'lucide-react';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export default function App() {
   return (
     <>
-      <Heading attr={123} attr2='String'>
+      <Heading>
         Olá mundo 1
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
 
       <p>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,10 @@
+import type React from 'react';
 import styles from './Heading.module.css';
 
-export function Heading(props) {
-  return <h1 className={styles.heading}>{props.children}</h1>;
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
