0f1c7bcf303abf09bdfc82e185eb789fb0288ba1
juliohtm08 <jhtomoyuki345@gmail.com>
2025-11-17 22:33:29 -0300

    Movendo o Container para seu próprio React Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export default function App() {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>LOGO</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>MENU</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FORM</section>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='content'>
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
