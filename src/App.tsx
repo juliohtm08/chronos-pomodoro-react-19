d11a955d2c1a24e08f7cb40eaa789629676ded30
juliohtm08 <jhtomoyuki345@gmail.com>
2025-11-17 22:14:15 -0300

    Criando a estrutura HTML e CSS do nosso app para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import { Heading } from './components/Heading';
-
-import { TimerIcon } from 'lucide-react';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export default function App() {
   return (
     <>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-        reprehenderit explicabo facilis eos dolor officiis, corrupti rerum
-        excepturi impedit at, corporis quo libero? Ex rerum hic corporis
-        inventore quae nemo.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
