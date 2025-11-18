3bd7e5ad74903fcc9b8bbdfa30d94f9059b26af5
juliohtm08 <jhtomoyuki345@gmail.com>
2025-11-17 23:14:39 -0300

    Criando o componente de Logo com ícone e link

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import './styles/theme.css';
-import './styles/global.css';
-
 import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export default function App() {
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
       </Container>
+
       <Container>
         <Heading>MENU</Heading>
       </Container>
