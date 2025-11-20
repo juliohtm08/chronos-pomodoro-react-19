0a797425070da1a444c0fad9e240afd345db94f6
juliohtm08 <jhtomoyuki345@gmail.com>
2025-11-20 10:41:58 -0300

    Entendendo o estado e o problema do efeito colateral

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,36 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { Footer } from './components/Footer';
+
+import { PlayCircleIcon } from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
 
 export default function App() {
+  let numero = 0;
+
+  function handleClick() {
+    const span = document.getElementById('numero');
+
+    if (!span) return;
+
+    numero += 1;
+    span.innerText = numero.toString();
+    console.log(numero, Date.now());
+  }
+
   return (
     <>
+      <Heading>
+        Número: <span id='numero'>{numero}</span>
+      </Heading>
+      <button onClick={handleClick}>Aumentar</button>
+
       <Container>
         <Logo />
       </Container>
